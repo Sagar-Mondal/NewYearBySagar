161a215620b9a62df2053208b56c00e3684880fc
Sagar Mondal <95748828+Sagar-Mondal@users.noreply.github.com>
2024-12-30 21:48:05 +0530

    Update newyear-fireworks-animation.js

--- newyear-fireworks-animation.js
+++ newyear-fireworks-animation.js
@@ -1,6 +1,6 @@
 let chars, particles, canvas, ctx, w, h, current;
 let duration = 5000;
-let str = ['HAPPY', 'NEW', 'YEAR' , '2025', 'BY', 'SAGARMONDAL'];
+let str = ['HAPPY', 'NEW', 'YEAR' , '2025', 'FROM', 'SAGARMONDAL'];
 
 init();
 resize();
